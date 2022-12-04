ca3730b2b1617f3ca9b718e35eaca708416ce601
Matias Dahl <matias.dahl@iki.fi>
2022-12-04 17:42:53 +0200

    first commit

--- workspace/mnist-demo-pipeline/mnist-demo-pipeline/reporting.py
+++ workspace/mnist-demo-pipeline/mnist-demo-pipeline/reporting.py
@@ -1,30 +1,64 @@
+from typing import Optional
+
+#
 import json
 from pathlib import Path
-from functools import lru_cache
+
+#
+from pynb_dag_runner.opentelemetry_helpers import Spans
+from pynb_dag_runner.opentelemetry_task_span_parser import get_pipeline_iterators
+
+from otel_output_parser.mermaid_graphs import (
+    make_mermaid_dag_inputfile,
+    make_mermaid_gantt_inputfile,
+)
 
 
-@lru_cache
 def args():
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
     parser.add_argument(
-        "--pipeline_outputs_path",
+        "--input_otel_spans_json_file",
         type=Path,
-        help="output path where OpenTelemetry spans have been expanded (tmp)",
+        help="input filepath with logged spans (as an OpenTelemetry JSON file) for pipeline run",
+    )
+    parser.add_argument(
+        "--output_markdown_file",
+        type=Path,
+        help="filepath where to write output markdown report",
     )
 
     return parser.parse_args()
 
 
-print(f"--- Command line parameters")
-print(f"  - pipeline_outputs_path         : {args().pipeline_outputs_path}")
+def load_spans() -> Spans:
+    return Spans(json.loads(args().input_otel_spans_json_file.read_text()))
 
 
-def get_url_to_this_run(pipeline_outputs_path: Path) -> str:
-    pipeline_attributes = json.loads(
-        (pipeline_outputs_path / "pipeline-outputs" / "pipeline.json").read_text()
-    )["attributes"]
+def get_pipeline_attributes(spans: Spans):
+    pipeline_metadata, _ = get_pipeline_iterators(spans)
+    return pipeline_metadata["attributes"]
+
+
+def is_remote_run(spans: Spans) -> bool:
+    """
+    Is the logged spans from a run a Github CI/CD pipeline?
+    """
+    return {
+        "pipeline.github.repository",
+        "pipeline.pipeline_run_id",
+    } <= get_pipeline_attributes(spans).keys()
+
+
+print(f"  - input_otel_spans_json_file  : {args().input_otel_spans_json_file}")
+print(f"  - output_markdown_file        : {args().output_markdown_file}")
+
+
+def get_url_to_this_run(spans: Spans) -> Optional[str]:
+    assert is_remote_run(spans)
+
+    pipeline_attributes = get_pipeline_attributes(spans)
 
     repo_owner, repo_name = pipeline_attributes["pipeline.github.repository"].split("/")
     run_id = pipeline_attributes["pipeline.pipeline_run_id"]
@@ -32,39 +66,49 @@ def get_url_to_this_run(pipeline_outputs_path: Path) -> str:
     return f"https://{repo_owner}.github.io/{repo_name}/#/experiments/all-pipelines-runs/runs/{run_id}"
 
 
-def make_markdown_report(pipeline_outputs_path: Path) -> str:
+def make_markdown_report(spans: Spans) -> str:
+    remote_run: bool = is_remote_run(spans)
     report_lines = []
 
-    runlink = get_url_to_this_run(pipeline_outputs_path)
-    report_lines.append(
-        f"Inspect details on this pipeline run: [Github Pages link]({runlink})"
-    )
-    report_lines.append("")
+    #
+    if remote_run:
+        runlink = get_url_to_this_run(spans)
+        report_lines.append(
+            f"Inspect details on this pipeline run: [Github Pages link]({runlink})"
+        )
+        report_lines.append("")
 
+    # Add DAG diagram
     report_lines.append("## DAG diagram of task dependencies in this pipeline")
     report_lines.append("```mermaid")
-    report_lines.append((pipeline_outputs_path / "dag.mmd").read_text())
+    report_lines.append(make_mermaid_dag_inputfile(spans, generate_links=remote_run))
     report_lines.append("```")
-    report_lines.append("Click on a task for more details.")
 
+    if remote_run:
+        report_lines.append("Click on a task for more details.")
+
+    # Add Gantt diagram
     report_lines.append("## Gantt diagram of task runs in pipeline")
     report_lines.append("```mermaid")
-    report_lines.append((pipeline_outputs_path / "gantt.mmd").read_text())
+    report_lines.append(make_mermaid_gantt_inputfile(spans))
     report_lines.append("```")
     report_lines.append("---")
-    report_lines.append(
-        "Note: the above links point to a static Github Pages site built using build artifacts "
-        "for this repo. The links will only work (1) after the static site has been built, and "
-        "(2) if the build artifacts existed when the site was last built. "
-        "Since Github Build artifacts has maximum retention period of 90 days, the "
-        "links will not work forever."
-    )
+
+    if remote_run:
+        report_lines.append(
+            "Note: the above links point to a static Github Pages site built using "
+            "build artifacts for this repo. The links will only work "
+            "(1) after the static site has been built, and "
+            "(2) if the build artifacts existed when the site was last built. "
+            "Since Github Build artifacts has maximum retention period of 90 days, "
+            "the links will not work forever."
+        )
 
     return "\n".join(report_lines)
 
 
-(args().pipeline_outputs_path / "pipeline_run_summary.md").write_text(
-    make_markdown_report(args().pipeline_outputs_path)
+args().output_markdown_file.write_text(
+    make_markdown_report(load_spans()),
 )
 
 
